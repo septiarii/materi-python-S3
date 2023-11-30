e6b1fd3b6668202ce7545259109fed19b407ce71
septiari <147464874+septiarii@users.noreply.github.com>
2023-11-30 08:38:29 +0800

    Add files via upload

---
+++ distionary_pada_list.py
@@ -0,0 +1,9 @@
+siswa = [
+    {"nama" : "upin", "tinggi" : 110, "berat" : 17},
+    {"nama" : "ipin", "tinggi" : 110, "berat" : 18},
+    {"nama" : "jarjit", "tinggi" : 112, "berat" : 19},
+    {"nama" : "melani", "tinggi" : 112, "berat" : 19},
+]
+
+for s in siswa:
+    print(f"{s['nama']} memiliki tinggi {s['tinggi']} cm, dan berat {s['berat']} kg")
